00bd2adb654ca36cbc825c798adc37d0361bdee0
Prestofall16 <162825888+Prestofall16@users.noreply.github.com>
2024-11-01 13:45:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('tema').addEventListener('click', function() {
+    document.getElementById('modo').href = 'css/light-mode.css';
+    alert('Tema mudado para Light Mode!');
+});
